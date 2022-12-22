c65619d599d5acfb8d40f5cd44a42ec571b7fe85
anton <onoshinanton@gmail.com>
2022-12-22 11:36:28 -0800

    main method created in class2

--- src/Class_2.java
+++ src/Class_2.java
@@ -1,9 +1,13 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Class_2 {
 
     public void method1(){
         System.out.println("Practicing");
     }
 
+    public static void main(String[] args) {
 
+    }
 
 }
